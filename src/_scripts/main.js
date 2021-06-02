67ceaac0b4db8c0eed6a022ec87f5643e24d0bd9
Nicoletta Bastianelli <nicoletta.bastianelli@gmail.com>
2021-06-02 20:51:10 +0200

    first element

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,12 +3,11 @@
 
 'use strict';
 
-import 'core-js';
-import 'regenerator-runtime/runtime';
-import $ from 'jquery';
-import { Link } from '../_modules/link/link';
+// import 'core-js';
+// import 'regenerator-runtime/runtime';
+// import $ from 'jquery';
+// import { Link } from '../_modules/link/link';
 
-$(() => {
-  new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
-});
+// $(() => {
+
+// });
